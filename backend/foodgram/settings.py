49c9a218a28354563128c226da4fd1d5f4f4ffa2
darda <dardali@mail.ru>
2023-06-15 15:18:48 +0300

    Проект

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -64,13 +64,8 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE',
-                            default="django.db.backends.postgresql"),
-        'NAME': os.getenv('DB_NAME', default="foodgram"),
-        'USER': os.getenv('POSTGRES_USER', default="foodgram_user"),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default="Daly09595"),
-        'HOST': os.getenv('DB_HOST', default="127.0.0.1"),
-        'PORT': os.getenv('DB_PORT', default="5432")
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
