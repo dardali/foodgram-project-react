815b4d487ae1113a0f753604543a78964b5c9ada
darda <dardali@mail.ru>
2023-06-26 21:17:37 +0300

    Проект

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,7 +3,6 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
